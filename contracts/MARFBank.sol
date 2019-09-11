9fce3962855035ff7bfa3f9cc481c2ce853f9c64
marf3927 <xogml3927@naver.com>
2019-09-11 13:51:01 +0900

    MARFBank interface

--- contracts/MARFBank.sol
+++ contracts/MARFBank.sol
@@ -3,27 +3,29 @@ pragma solidity ^0.5.6;
 import '@openzeppelin/contracts/ownership/Ownable.sol';
 
 contract MARFBank is Ownable {
-	mapping(address => uint256) private balance;
-	bool private bankOpen = true;
+	modifier bankOpened {
+		_;
+	}
 
-	modifier bankOpened() {
+	function setBankState(bool bankOpen) public {
 
 	}
 
-	function setBankState(bool _bankOpen) external onlyOwner {
-	}
-
-	function deposit(uint256 _amount) external payable bankOpened {
-	}
-
-	function withdraw(uint256 _amount) external bankOpened {
-	}
-
-	function transfer(address _to, uint256 _amount) external bankOpened {
+	function deposit(uint256 amount) public {
 
 	}
 
-	function getBalance(address _user) external view bankOpened returns (uint256) {
-		return balance[_user];
+	function withdraw(uint256 amount) public {
+
 	}
+
+	function transfer(address to, address from, address amount) public {
+
+	}
+
+	function getBalace(address user) public return (uint256){
+		return 0;
+	}
+
+
 }
